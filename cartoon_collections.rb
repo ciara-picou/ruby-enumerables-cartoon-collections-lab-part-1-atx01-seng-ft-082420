702287c0aece1c1e5a8af36758285b9c05993636
Ciara Picou <ciarapicou@att.net>
2020-07-23 22:11:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,14 +15,3 @@ def list_dwarves(array)
   p "#{index + 1}". element
 }
 end
-each_with_index(*args) { |obj, i| block } → enumclick to toggle source
-each_with_index(*args) → an_enumerator
-Calls block with two arguments, the item and its index, for each item in enum. Given arguments are passed through to each().
-
-If no block is given, an enumerator is returned instead.
-
-hash = Hash.new
-%w(cat dog wombat).each_with_index { |item, index|
-  hash[item] = index
-}
-hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
